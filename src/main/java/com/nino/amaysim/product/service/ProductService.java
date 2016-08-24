fe7a5bd241a1188147e5af42bb8469563db1db86
nbret00 <nino.bretana@safeway.com>
2016-08-24 13:16:48 +0800

    Update ProductService.java

--- src/main/java/com/nino/amaysim/product/service/ProductService.java
+++ src/main/java/com/nino/amaysim/product/service/ProductService.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.nino.amaysim.product.service;
 
 /**
  *
- * @author nbret00
+ * @author nbret0o of product exposing product and products method as a REST service
+ * 
+ * Controller o
  */
 
 import com.nino.amaysim.product.Product;
